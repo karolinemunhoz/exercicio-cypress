b7c9d2351743a813b3b0c829c6b8da3fd332a8ad
Karoline <karolinemunhozcontato@gmail.com>
2026-02-14 01:46:55 -0300

    conserta teste cypress

--- cypress/e2e/ebac-agenda/teste.cy.js
+++ cypress/e2e/ebac-agenda/teste.cy.js
@@ -2,34 +2,35 @@
 
 describe('Testes para adicionar, alterar e remover contato', () => {
 
-    beforeEach(() => {
-        cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
-    })
+  beforeEach(() => {
+    cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
+  })
 
-    it('Deve adicionar um contato', () => {
-        cy.get('input[type="text"]').type('Karoline Munhoz')
-        cy.get('input[type="email"]').type('karoline@gmail.com')
-        cy.get('input[type="tel"]').type('21999999999')
-        cy.get('.adicionar').click()
+  it('Deve adicionar um contato', () => {
+    cy.get('input[type="text"]').type('Karol Teste')
+    cy.get('input[type="email"]').type('karolteste@gmail.com')
+    cy.get('input[type="tel"]').type('21999999999')
+    cy.get('.adicionar').click()
 
-        cy.get('.contato').should('have.length.greaterThan', 0)
-    })
+    cy.contains('Karol Teste').should('exist')
+  })
 
-    it('Deve alterar um contato', () => {
-        cy.get('.edit').first().click()
+  it('Deve alterar um contato', () => {
+    cy.get('.edit').first().click()
 
-        cy.get('input[type="text"]').clear().type('Maria da Silva')
-        cy.get('input[type="email"]').clear().type('maria@gmail.com')
-        cy.get('input[type="tel"]').clear().type('21988888888')
-        cy.get('.alterar').click()
+    cy.get('input[type="text"]').clear().type('Contato Editado')
+    cy.get('input[type="email"]').clear().type('editado@gmail.com')
+    cy.get('input[type="tel"]').clear().type('21888888888')
 
-        cy.get('.contato').should('exist')
-    })
+    cy.get('.alterar').click()
 
-    it('Deve remover um contato', () => {
-        cy.get('.delete').first().click()
+    cy.contains('Contato Editado').should('exist')
+  })
 
-        cy.get('.contato').should('exist')
-    })
+  it('Deve remover um contato', () => {
+    cy.get('.delete').first().click()
+
+    cy.get('.contato').should('exist')
+  })
 
 })
